1185c460cc60639d98e63b0e560e9c0cd635d309
lanlehoang67 <lanlehoang10@gmail.com>
2018-11-22 15:39:28 +0700

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_11_20_065013) do
 
-ActiveRecord::Schema.define(version: 2018_11_20_123614) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
