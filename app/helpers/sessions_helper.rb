3ab6773e198e3034c70536b8f77e079d998fffe3
lanlehoang67 <lanlehoang10@gmail.com>
2018-11-25 18:07:06 +0700

    m

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 module SessionsHelper
 	def log_in(user)
 		session[:user_id]=user.id
-		
+
+	end
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[]
 	end
 	def current_user
 		if session[:user_id]
